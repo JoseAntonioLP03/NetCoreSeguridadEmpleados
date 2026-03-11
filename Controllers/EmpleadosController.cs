cb97c905d776c5f0286a1e7e91378317f5a3cc41
JoseAntonioLP03 <l.p.joseantonio03@gmail.com>
2026-03-11 14:09:10 +0100

    Add project files.

---
+++ Controllers/EmpleadosController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreSeguridadEmpleados.Filters;
+using NetCoreSeguridadEmpleados.Models;
+using NetCoreSeguridadEmpleados.Repositories;
+using System.Security.Claims;
+
+namespace NetCoreSeguridadEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public EmpleadosController (RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            List<Empleado> empleados = await this.repo.GetEmpleadosAsync();
+            return View(empleados);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Empleado empleado = await this.repo.FindEmpleadoAsync(id);
+            return View(empleado);
+        }
+        [AuthorizeEmpleados]
+        public IActionResult PerfilEmpleado()
+        {
+            return View();
+        }
+
+        [AuthorizeEmpleados]
+
+        public async Task<IActionResult> Compis()
+        {
+            //RECUPERAMOS EL CLAIM DEL USUARIO VALIDADO
+            string dato = HttpContext.User.FindFirstValue("Departamento");
+            int idDepartamento = int.Parse(dato);
+            List<Empleado> empleados = await this.repo.GetEmpleadosDepartamentoAsync(idDepartamento);
+            return View(empleados);
+        }
+    }
+}
---
+++ Controllers/ManagedController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreSeguridadEmpleados.Models;
+using NetCoreSeguridadEmpleados.Repositories;
+using System.Security.Claims;
+
+namespace NetCoreSeguridadEmpleados.Controllers
+{
+    public class ManagedController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public ManagedController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> Login(string username , string password)
+        {
+            int idEmpleado = int.Parse(password);
+            Empleado empleado = await this.repo.LogInEmpleadoAsync(username, idEmpleado);
+            if(empleado != null)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+
+                Claim claimName = new Claim(ClaimTypes.Name, username);
+                identity.AddClaim(claimName);
+
+                Claim claimId = new Claim(ClaimTypes.NameIdentifier, empleado.IdEmpleado.ToString());
+                identity.AddClaim(claimId);
+
+                //COMO ROLE UTILZIAMOS EL OFICIO
+                Claim claimRole = new Claim(ClaimTypes.Role, empleado.Oficio);
+                identity.AddClaim(claimRole);
+
+                Claim claimSalario = new Claim("Salario", empleado.Salario.ToString());
+                identity.AddClaim(claimSalario);
+
+                Claim claimDept = new Claim("Departamento", empleado.IdDepartamento.ToString());
+                identity.AddClaim(claimDept);
+
+                //COMO POR AHORA NO VOY A UTILIZAR ROLES , NO LO INDICAMOS
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+                //POR AHORA LO ENVIAMOS A UNA VISTA QUE VEREMOS EN BREVE
+                return RedirectToAction("PerfilEmpleado", "Empleados");
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Credenciales incorrectas";
+                return View();
+            }
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult ErrorAcceso()
+        {
+            return View();
+        }
+    }
+}
+ 
